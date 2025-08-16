ebcba181ddfcf5e581ce8b4857c5ad42a67e11f6
duetue8 <duetue8@gmail.com>
2025-08-16 01:44:14 -0700

    Publish application

---
+++ dist/assets/browser-ChABX77f.js
@@ -0,0 +1,2 @@
+import{e}from"./react-vendor-2C4eXZrv.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
+//# sourceMappingURL=browser-ChABX77f.js.map
